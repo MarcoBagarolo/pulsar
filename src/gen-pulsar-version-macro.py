ff76e8739e3414371a1dede07792218731b17c8f
MarcoBagarolo <117910175+MarcoBagarolo@users.noreply.github.com>
2023-11-13 12:12:11 +0100

    Update gen-pulsar-version-macro.py

--- src/gen-pulsar-version-macro.py
+++ src/gen-pulsar-version-macro.py
@@ -17,7 +17,7 @@
 # specific language governing permissions and limitations
 # under the License.
 #
-
+import random
 import xml.etree.ElementTree as ET
 import re
 from os.path import dirname, realpath, join
